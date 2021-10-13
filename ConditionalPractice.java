bbe4369b37136d27fd72d4a297916b9c1a198101
antwanette <lillefty01@aol.com>
2021-10-13 16:03:23 -0400

    Practice with Conditional Statements

---
+++ ConditionalPractice.java
@@ -0,0 +1,15 @@
+
+public class ConditonalFun {
+	 public static void main(String[] args){
+         int t=32;
+
+         if (t < 40){
+         System.out.println("Wear a parka!");
+         }
+
+         else if (t>=40) {
+         System.out.println("Wear a light jacket.");
+         }
+         
+}// end of main
+}
